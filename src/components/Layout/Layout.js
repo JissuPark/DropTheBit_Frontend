12591bbe57789803adc3bd0fa340930c3bc420fe
JissuPark <qkrwltn9412@gmail.com>
2021-04-26 21:24:56 +0900

    [ADD] All

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -9,7 +9,7 @@ import UIIcons from '../../pages/components/icons';
 import UINotifications from '../../pages/notifications';
 import TablesStatic from '../../pages/tables/static';
 import MapsGoogle from '../../pages/components/maps/google';
-import CoreTypography from '../../pages/typography';
+import Typography from '../../pages/typography';
 import Charts from '../../pages/components/charts/Charts';
 import Dashboard from '../../pages/dashboard';
 
@@ -83,7 +83,7 @@ class Layout extends React.Component {
                     <Route path="/app/components/charts" exact component={Charts} />
                     <Route path="/app/tables" exact component={TablesStatic} />
                     <Route path="/app/components/maps" exact component={MapsGoogle} />
-                    <Route path="/app/typography" exact component={CoreTypography} />
+                    <Route path="/app/typography" exact component={Typography} />
                   </Switch>
                 </CSSTransition>
               </TransitionGroup>
